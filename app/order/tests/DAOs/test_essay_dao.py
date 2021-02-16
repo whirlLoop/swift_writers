7bafa4d7673de7872004565a8766c512617b447c
xcixor <pndungu54@gmail.com>
2021-02-16 11:18:04 +0300

    refactor(tests): abstract common use cases for DAO tests

--- app/order/tests/DAOs/test_essay_dao.py
+++ app/order/tests/DAOs/test_essay_dao.py
@@ -1,25 +1,16 @@
 """Tests essay data access object.
 """
-import json
-from django.test import TestCase
-from django.conf import settings
-from django_redis import get_redis_connection
 from order.DAOs.essay_dao import EssayDAO
 from order.domain_objects.essay_object import EssayObject
+from order.tests.common.base_test import BaseTestCase
 
 
-class EssayDAOTestCase(TestCase):
+class EssayDAOTestCase(BaseTestCase):
 
     def setUp(self) -> None:
-        get_redis_connection("default").flushall()
-        self.cache = get_redis_connection()
-        root_dir = str(settings.BASE_DIR)[:-13]
-        json_file_path = root_dir + 'order/tests/data/essays.json'
-        with open(json_file_path) as f:
-            data = json.loads(f.read())
-            self.cache.set('essays', json.dumps(data))
+        super(EssayDAOTestCase, self).setUp()
+        self.set_cache_from_json_file('order/tests/data/essays.json')
         self.dao_instance = EssayDAO()
-        return super().setUp()
 
     def test_returns_a_list_of_essays(self):
         self.assertIsInstance(
@@ -36,6 +27,3 @@ class EssayDAOTestCase(TestCase):
             'Argumentative essay',
             str(self.cache.get("essays"))
         )
-
-    def tearDown(self):
-        get_redis_connection("default").flushall()
---
+++ app/order/tests/common/base_test.py
@@ -0,0 +1,24 @@
+"""BaseTestCase
+    """
+import json
+from django.test import TestCase
+from django.conf import settings
+from django_redis import get_redis_connection
+
+
+class BaseTestCase(TestCase):
+
+    def setUp(self) -> None:
+        get_redis_connection("default").flushall()
+        self.cache = get_redis_connection()
+        self.root_dir = str(settings.BASE_DIR)[:-13]
+        return super().setUp()
+
+    def set_cache_from_json_file(self, json_file_path):
+        file_path = self.root_dir + json_file_path
+        with open(file_path) as f:
+            data = json.loads(f.read())
+            self.cache.set('essays', json.dumps(data))
+
+    def tearDown(self):
+        get_redis_connection("default").flushall()
