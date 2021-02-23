3b6690b9e98abe4267d2082203f4736d015ca182
xcixor <pndungu54@gmail.com>
2021-02-23 19:31:03 +0300

    chore(ci/cd): add env variables for email testing on circle ci

---
+++ scripts/create_env_file.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+create_env_file() {
+    touch .env
+    echo "EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}" >> .env
+    echo "VERIFIED_EMAIL_USER=${VERIFIED_EMAIL_USER}" >> .env
+}
+
+main(){
+    create_env_file
+}
+
+main
