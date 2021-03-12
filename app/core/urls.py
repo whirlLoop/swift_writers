e86765af0afd03e01b2d70ab4e99e9037fa8ffd6
xcixor <pndungu54@gmail.com>
2021-03-12 18:36:30 +0300

    chore(content): style login form

--- app/core/urls.py
+++ app/core/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,3 +27,7 @@ urlpatterns = [
         'authentication.presentation.urls', namespace='authentication'),
     ),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL,
+                            document_root=settings.STATIC_ROOT)
