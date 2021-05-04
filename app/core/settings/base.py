9a356cc006cdfbe686477e2fceb776a35670808f
xcixor <pndungu54@gmail.com>
2021-05-04 19:25:31 +0300

    chore(order materials): save order materials
     - delete temp materials to prevent resource wastage
    
    - add dependency to store user materials to aid in transfer
    
     - sync tests

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_mysql',
     'order.apps.OrderConfig',
     'resources.apps.ResourcesConfig',
     'swift_writers.apps.SwiftWritersConfig',
